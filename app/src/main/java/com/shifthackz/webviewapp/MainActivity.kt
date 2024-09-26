c754ef5b30391373b836281f7c4c53e92be5283f
Luke Steuber <luke@camina.local>
2024-09-26 13:01:45 -0600

    working right now with orb errors

--- app/src/main/java/com/shifthackz/webviewapp/MainActivity.kt
+++ app/src/main/java/com/shifthackz/webviewapp/MainActivity.kt
@@ -1,19 +1,21 @@
 package com.shifthackz.webviewapp
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.*
 import android.widget.Toast
-import androidx.activity.OnBackPressedCallback
 import androidx.activity.addCallback
 import androidx.annotation.RequiresApi
+import androidx.browser.customtabs.CustomTabsIntent
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
-        private const val APP_URL = "https://moroz.cc"
+        private const val APP_URL = "https://ai.assisted.space"
     }
 
     private val appWebViewClient = object : WebViewClient() {
@@ -26,6 +28,23 @@ class MainActivity : AppCompatActivity() {
         ) {
             super.onReceivedError(view, request, error)
             error?.description?.toString()?.let(::showError)
+        }
+
+        override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+            val url = request?.url?.toString()
+            if (url != null) {
+                // Check for Google Sign-In or other secure operations
+                if (url.startsWith("https://accounts.google.com") ||
+                    url.startsWith("your_other_secure_url_prefix")) {
+                    // Open these URLs in a Custom Tab or external browser
+                    openInCustomTab(url)
+                    return true
+                } else {
+                    // For other URLs, let the WebView handle them
+                    return false // This is important to enable JavaScript and other features
+                }
+            }
+            return false
         }
     }
 
@@ -43,9 +62,18 @@ class MainActivity : AppCompatActivity() {
 
         webView.apply {
             settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true // Enable DOM storage for JavaScript
+            settings.javaScriptCanOpenWindowsAutomatically = true // Allow JavaScript to open windows
             webViewClient = appWebViewClient
+            webView.settings.safeBrowsingEnabled = false
             loadUrl(APP_URL)
         }
+    }
+
+    private fun openInCustomTab(url: String) {
+        val builder = CustomTabsIntent.Builder()
+        val customTabsIntent = builder.build()
+        customTabsIntent.launchUrl(this, Uri.parse(url))
     }
 
     private fun showError(error: String) {
